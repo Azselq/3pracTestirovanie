44329982e18c235436d9d206c77f8f35ede0e94c
Azselq <denok2002@gmail.com>
2022-10-11 04:46:12 +0300

    Начало создания проекта

---
+++ 3prac/src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(){
+    val therapist = Therapist()
+    therapist.setClientName()
+    therapist.setClientSex()
+    therapist.setClientAge()
+    therapist.setClientPainType()
+    therapist.healing()
+}
